309c4d62dd0f224659b07a31f4f027e506a25f91
LeticiaMascarenhas01 <lsmascarenhas20@gmail.com>
2025-03-11 00:27:01 -0300

    atividade conta banco

--- Conta-Banco/src/ContaTerminal.java
+++ Conta-Banco/src/ContaTerminal.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        try (Scanner banco = new Scanner(System.in)) {
+            System.out.println("Digite o seu nome e sobrenome: ");
+            String nome = banco.nextLine();
+
+            System.out.println("Por favor, digite o número da Agência !");
+            int agencia = banco.nextInt();
+
+            System.out.println("Por favor, digite seu saldo: ");
+            double saldo = banco.nextDouble();
+
+            System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + saldo + " e seu saldo " + saldo + " já está disponível para saque.");
+        }
     }
 }
